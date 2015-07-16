09796df7ee7e3b936e4ded319af53529267a3ce1
LauraLaureus <l4depende@gmail.com>
2015-07-16 12:05:51 +0100

    Setup Example

---
+++ twistGasket-example.js
@@ -0,0 +1,10 @@
+var gl;
+var points;
+
+window.onload = function init() {
+	var canvas = document.getElementById('gl-canvas');
+	gl = WebGLUtils.setupWebGL(canvas);
+	if(!gl){
+		alert("WebGL isn't available");
+	}
+}
