4becb422617da484ae73421900354eed8a861af4
LauraLaureus <l4depende@gmail.com>
2015-07-16 20:34:23 +0100

    example corrections

--- twistGasket-example.js
+++ twistGasket-example.js
@@ -1,30 +1,54 @@
+"use strict";
+
 var gl;
 var points;
 
-window.onload = function init() {
-	var canvas = document.getElementById('gl-canvas');
-	gl = WebGLUtils.setupWebGL(canvas);
-	if(!gl){
-		alert("WebGL isn't available");
-	}
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
 
-	var vertices = [vec2(-0.5,-0.5), vec2(-0.5.0.5),vec2(0.5,0.5),vec2(0.5,0.5)];
-	gl.viewpor(0,0,canvas.width, canvas.height);
-	gl.clearColor(0.0,0.0,0.0,1.0);
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-	var program = initShaders(gl,"vertex-shader","fragment-shader");
-	gl.useProgram(program);
 
-	//Process in GPU -------------------------------------
-	//Create a vertex Buffer Object on GPU
-	var bufferID = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER,bufferID);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices),gl.STATIC_DRAW);
+    // Four Vertices
 
-	//Flatten is a function defined in MV.js, it transforms JS array to float32 array
+    var vertices = [
+      -0.5, -0.5,
+      -0.5,  0.5,
+      0.5, 0.5,
+      0.5, -0.5
+    ];
 
-	//Assosiate shader variables with variables in JS
-	var vPosition = gl.getAttribLocation(program,"vPosition");
-	gl.vertexAttribPointer(vPosition,2,gl.FLOAT,false,0,0);
-	gl.enableVertexAttribArray(vPosition);
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
 }
