090c58b7a058de0817687e73fd52bdad68c79e29
LauraLaureus <l4depende@gmail.com>
2015-07-17 12:16:28 +0100

    Draw a red triangle

--- twistGasket.js
+++ twistGasket.js
@@ -1,40 +1,44 @@
-"use strict";
 
 var gl;
 var points;
 
-window.onload = function init(){
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-	//Setup canvas where application will render
-	var canvas = document.getElementById("gl-canvas");
-	gl = WebGLUtils.setupWebGL(canvas);
+    
+    var vertices = [-1, -1, 0, 1, 1, -1];
 
-	//Placing the triangle vertices
-	var vertices = new Float32Array([-1,1,0,1,1,-1]);
+    //  Configure WebGL
 
-	//Configure WebGL
-	gl.viewport(0,0,canvas.width,canvas.height);
-	gl.clearColor(1.0,1.0,1.0,1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
-	//Load shaders and initialize attribute buffers.
-	var program = initShaders(gl,"vertex-shader","fragment-shader");
-	gl.useProgram(program);
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
-	//Load data into GPU
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER,bufferId);
-	gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW);
+    render();
+};
 
-	//Associate out shader variables with our data buffer
-	var vPosition = gl.getAttrbLocation(program, "vPosition");
-	gl.vertexAttribPointer(vPosition,2,gl.FLOAT,false,0,0);
-	gl.enableVertexAttribArray(vPosition);
 
-	render();
-}
-
-function render(){
-
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.triangle,0,3);
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
 }
